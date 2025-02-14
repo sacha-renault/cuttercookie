a3420d4620c2f50032fc5f67e00b7dec4a096725
Sacha RENAULT <sacha35340@gmail.com>
2025-02-14 10:32:03 +0100

    init project

---
+++ src/cli/cli_args.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "fs-tools")]
+#[command(about = "File system utility tools", long_about = None)]
+pub struct Cli {
+    /// Directory path to start from
+    #[arg(default_value = ".")]
+    path: PathBuf,
+}
---
+++ src/cli/mod.rs
@@ -0,0 +1,9 @@
+pub mod cli_args;
+
+use clap::Parser;
+
+use cli_args::Cli;
+
+pub fn entry_point() {
+    let args = Cli::parse();
+}
